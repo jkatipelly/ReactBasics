d6a0bc11f917a114082bdcd5d3fd57fab0dbbe91
Jalander Reddy <Jalander.Reddy@gmail.com>
2022-09-13 11:48:10 +0100

    intial status

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+
+class Header extends Component{
+    render()
+    {
+        return React.createElement('Div',{id:'hello'},'Class without JSX')
+            
+        
+    }
+}
+
+export default Header
