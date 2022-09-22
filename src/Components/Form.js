dc5803fb9189115a02cef40e946f21edb2295a6b
Jalander Reddy <jkatipelly@yahoo.com>
2022-09-22 16:12:16 +0100

    Form Handling using controlled Components

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,13 +1,72 @@
 import React, { Component } from 'react'
 
 class Form extends Component {
+
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       username : '',
+       comments : '',
+       topic : 'React'
+
+    }
+  }
+
+  handleUserNameChange = (event)=>
+  {
+    this.setState({
+      username: event.target.value
+    })
+  }
+
+  handleCommentsChange = (event) =>
+  {
+    this.setState({
+      comments : event.target.value
+    })
+  }
+
+  handleTopicChange = (event)=>
+  {
+    this.setState({
+      topic:event.target.value
+    })
+  }
+
+  handleSubmit=(event)=>{
+
+    alert(`${this.state.username} ${this.state.comments} ${this.state.topic}`)
+    event.preventDefault()
+  }
+  
   render() {
+
+    const{username, comments, topic}=this.state
+
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <div>
             <label>Username</label>
-            <input type='text' />
+            <input type='text' 
+                   value={username} 
+                   onChange={this.handleUserNameChange}/>            
         </div>
+        <div>
+          <label>Comments</label>
+          <textarea value={comments} onChange={this.handleCommentsChange}></textarea>
+        </div>
+
+        <div>
+          <label>Topic</label>
+          <select value={topic} onChange={this.handleTopicChange}>
+            <option value='Angular'>Angular</option>
+            <option value='React'>React</option>
+            <option value='Vue'>Vue</option>
+          </select>
+        </div>
+
+        <button type='submit'>Submit</button>
       </form>
     )
   }
